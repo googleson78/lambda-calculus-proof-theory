e6d6923c73cee71b3c63311d2f547040af37bd74
Georgi Lyubenov <godzbanebane@gmail.com>
2018-03-15 18:18:04 +0200

    Updated untyped arithmetic
    Added basic functions such as depth.
    Added single-step reduction (reduce) and multi-step reduction (eval).
    Maybe this could use refactoring for readability.

--- untyped-arithmetic.hs
+++ untyped-arithmetic.hs
@@ -1,8 +1,49 @@
 data Term = Tru
-          | Falsch
+          | Fls
           | Cond Term Term Term
           | Zero
           | Succ Term
           | Pred Term
           | IsZero Term
             deriving (Show, Eq)
+
+consts :: Term -> [Term]
+consts Tru          = [Tru]
+consts Fls          = [Fls]
+consts (Cond x y z) = concatMap consts [x, y, z]
+consts Zero         = [Zero]
+consts (Succ x)     = consts x
+consts (Pred x)     = consts x
+consts (IsZero x)   = consts x
+
+size :: Term -> Int
+size Tru          = 1
+size Fls          = 1
+size (Cond x y z) = succ $ sum $ map size [x, y, z]
+size Zero         = 1
+size (Succ x)     = succ $ size x
+size (Pred x)     = succ $ size x
+size (IsZero x)   = succ $ size x
+
+depth :: Term -> Int
+depth Tru          = 1
+depth Fls          = 1
+depth (Cond x y z) = succ $ maximum $ map depth [x, y, z]
+depth Zero         = 1
+depth (Succ x)     = succ $ depth x
+depth (Pred x)     = succ $ depth x
+depth (IsZero x)   = succ $ depth x
+
+reduce :: Term -> Either String Term
+reduce (Cond Tru y z) = Right y
+reduce (Cond Fls y z) = Right z
+reduce (Cond x y z)   = let xval = reduce x in
+                          (\t -> Cond t y z) <$> xval
+reduce _              = Left "Does not reduce"
+
+eval :: Term -> Term
+eval = either error id . eval' . pure
+
+eval' :: Either String Term -> Either String Term
+eval' t = let next = reduce =<< t in
+            either (const t) (eval' . pure) next
